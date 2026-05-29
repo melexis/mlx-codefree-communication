5f5ff40e6b1caa7bff8a856c786bb72225f15f58
jnh-rgb <jnh@melexis.com>
2026-05-29 08:44:14 +0000

    change #pragma error to C standard
    Fix compiler directive: replace #pragma error with #error
    Fixes #3

--- port/cmsis/codefree_comm_cmsis_uart.c
+++ port/cmsis/codefree_comm_cmsis_uart.c
@@ -40,8 +40,7 @@
 
 /* Define default driver instance if not provided by build system */
 #ifndef CODEFREE_COMM_USART_DRIVER_INSTANCE
-#pragma error(                                                                 \
-    "define a CODEFREE_COMM_USART_DRIVER_INSTANCE for the mlx codefree communication block.")
+#error "define a CODEFREE_COMM_USART_DRIVER_INSTANCE for the mlx codefree communication block."
 #endif
 
 extern ARM_DRIVER_USART ARM_Driver_USART_(CODEFREE_COMM_USART_DRIVER_INSTANCE);
